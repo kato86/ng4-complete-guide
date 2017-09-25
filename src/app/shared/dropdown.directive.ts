b7cc299958d01a7295f38af5d26502e648a7f84e
kamil <tomala.kamil@gmail.com>
2017-09-25 09:19:50 +0200

    Accept changes

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,21 +1,14 @@
-<<<<<<< HEAD
-import { Directive } from '@angular/core';
-=======
 import { Directive, HostListener, HostBinding } from '@angular/core';
->>>>>>> cf7f3891a2114b3b5f4c2d6701e070dde84a8074
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
-<<<<<<< HEAD
-=======
   @HostBinding('class.open') isOpen = false;
 
   @HostListener('mouseenter') toggleOpen() {
     this.isOpen = !this.isOpen;
   }
->>>>>>> cf7f3891a2114b3b5f4c2d6701e070dde84a8074
 
   constructor() { }
 
