74d114b7004ef23392ef5ecb351bb1ce2935d3ef
kamil <tomala.kamil@gmail.com>
2017-09-29 11:00:22 +0200

    using service for push

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from '@angular/core';
 import { Ingredient } from './../shared/ingredient.model';
 
 export class ShoppingListService {
+
+  ingredientChange = new EventEmitter<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -15,6 +18,7 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
   this.ingredients.push(ingredient);
+  this.ingredientChange.emit(this.ingredients.slice());
 }
 
 }
