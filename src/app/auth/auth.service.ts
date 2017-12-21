29e3420bf15d675d1137ab4103446b19ec6cc082
kamil <tomala.kamil@gmail.com>
2017-12-21 09:18:29 +0100

    Sending the token

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { resolve } from 'url';
 import { messaging } from 'firebase';
 
 export class AuthService {
+    token: string;
 
     singupUser(email: string, password: string) {
         firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -14,10 +15,23 @@ export class AuthService {
     singinUser(email: string, password: string) {
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(
-                resolve => console.log(resolve)
+                response => {
+                   firebase.auth().currentUser.getToken()
+                       .then(
+                           (token: string) => this.token = token
+                       );
+                }
             )
             .catch(
                 error => alert(error)
             );
+    }
+
+    getToken() {
+        firebase.auth().currentUser.getToken()
+            .then(
+                (token: string) => this.token = token
+            );
+        return this.token;
     }
 }
